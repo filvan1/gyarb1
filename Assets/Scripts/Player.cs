432e62f7efb990e78d4fe6dd3e93743232c2399e
filvan1 <filip.vannfalt@gmail.com>
2016-08-31 10:22:11 +0200

    E1
    Bas

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+
+	float gravity=-20;
+	Vector3 velocity;
+
+	Controller2D controller;
+
+
+
+	void Start () {
+		controller=GetComponent<Controller2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
